cffeaa2396f15e899b49ca7e9a20c8e24c543804
SophieDel <sophie.demeulemeester@gmail.com>
2022-10-07 10:26:41 +0200

    [hackatweet-backend] adjust /tweets/new routee

--- routes/tweets.js
+++ routes/tweets.js
@@ -2,17 +2,26 @@ var express = require("express");
 var router = express.Router();
 const Tweet = require('../models/tweets');
 
+
+router.get("/", (req, res) => {
+  Tweet.find().then(data => 
+    res.json({result: true, tweets: data}))
+    .catch(error => console.log(error))
+})
+
+
 router.post("/new", (req, res) => {
-  const rawTweet = req.body.content;
+  console.log("req.body.tweet =>", req.body.tweet)
+  const rawTweet = req.body.tweet;
   const hashtagPattern = /#[a-z0-9_]+/gi;
   // Isoler les hashtages du tweet
   const hashtagsArray = rawTweet.match(hashtagPattern);
   // Supprimer les hashtag du content
-  const content = rawTweet.split(" ").filter((e) => !hashtagsArray.includes(e)).join(" ");
+  const content = hashtagsArray ? rawTweet.split(" ").filter((e) => !hashtagsArray.includes(e)).join(" ") : rawTweet
   // Chaque hashtag est unique
-  const hashtags = [
-    ...new Set(hashtagsArray.map((e) => e.slice(1).toLowerCase())),
-  ];
+  const hashtags = hashtagsArray ? [
+    ...new Set(hashtagsArray.map((e) => e.slice(1).toLowerCase()))
+  ] : [];
   const newTweet = new Tweet({
     rawTweet: rawTweet,
     content: content,
